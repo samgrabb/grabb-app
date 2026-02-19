0d1c0a7632494f01aa157e41be3e454c06d397a5
Sam <sam@grabb.ch>
2026-02-19 22:53:04 +0100

    Fix: Show status bar properly with grabb.ch colors

--- android/app/src/main/java/ch/grabb/app/MainActivity.java
+++ android/app/src/main/java/ch/grabb/app/MainActivity.java
@@ -1,5 +1,26 @@
 package ch.grabb.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Show status bar with dark icons
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        
+        // Set status bar color
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark, getTheme()));
+        
+        // Light status bar (dark icons)
+        View decorView = getWindow().getDecorView();
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), decorView);
+        controller.setAppearanceLightStatusBars(false);
+    }
+}
